2ed442d7ebb284ed5c726f58e4a58b17426d1f2e
James Lyne <jim+github@not-null.co.uk>
2025-06-22 01:50:57 +0100

    Cleanup repo
    - Move core to top level

--- SUPPORT.md
+++
@@ -1,4 +0,0 @@
-# Discord
-
-It is preferred to use our [Discord](https://discord.gg/KxkjDVg)
-to get Hyperverse support
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,1 @@
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-    }
-    includeBuild("gradle/build-logic")
-}
-
 rootProject.name = "Hyperverse"
-
-include(":hyperverse-core")
