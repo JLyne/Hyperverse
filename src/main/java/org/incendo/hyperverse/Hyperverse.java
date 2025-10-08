92d5849b23fc955e6d26224e47d45999e85f365c
James Lyne <jim+github@not-null.co.uk>
2025-10-08 15:54:25 +0100

    1.21.10

--- src/main/java/org/incendo/hyperverse/Hyperverse.java
+++ src/main/java/org/incendo/hyperverse/Hyperverse.java
@@ -81,7 +81,7 @@ public final class Hyperverse extends JavaPlugin implements HyperverseAPI, Liste
     private final ServicePipeline servicePipeline = ServicePipeline.builder().build();
 
     private final List<String> supportedVersions = List.of(
-            "1.21.9"
+            "1.21.10"
     );
 
     private WorldManager worldManager;
